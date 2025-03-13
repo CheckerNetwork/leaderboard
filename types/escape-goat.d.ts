ba84e0db003d51d1d0d8860be50bfccdf3f83d1c
Srdjan S <stankovic.srdjo@gmail.com>
2025-03-13 09:33:56 +0100

    Remove custom type definitions

--- types/escape-goat.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'https://cdn.jsdelivr.net/npm/escape-goat@4.0.0/+esm' {
-  export function htmlEscape(template: TemplateStringsArray, ...substitutions: readonly unknown[]): string;
-}
